5cda77ddb77b7c8d7fb5c10c9b741853f5ffe44a
JorgeAChacon <62924612+JorgeAChaconL@users.noreply.github.com>
2025-02-05 22:58:25 -0600

    Format the doc to simplify the lecture

--- main.js
+++ main.js
@@ -1,123 +1,136 @@
-class Media{
-    constructor(title){
-        this._title = title;
-        this._isCheckedOut = false;
-        this._ratings = [];
-    }
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
 
-    get title(){
-        return this._title;
-    };
+  get title() {
+    return this._title;
+  }
 
-    get isCheckedOut(){
-        return this._isCheckedOut;
-    };
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
 
-    get ratings(){
-        return this._ratings;
-    };
+  get ratings() {
+    return this._ratings;
+  }
 
-    set isCheckedOut(value){
-        this._isCheckedOut = value;
-    };
+  set isCheckedOut(value) {
+    this._isCheckedOut = value;
+  }
 
-    toggleCheckOutStatus(){
-        this._isCheckedOut = !this._isCheckedOut;
-    };
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this._isCheckedOut;
+  }
 
-    getAverageRating(){
-        let ratingsSum = this._ratings.reduce((currentSum, rating) => currentSum + rating,0);
-        return ratingsSum / this._ratings.length;
-    }
+  getAverageRating() {
+    let ratingsSum = this._ratings.reduce(
+      (currentSum, rating) => currentSum + rating,
+      0
+    );
+    return ratingsSum / this._ratings.length;
+  }
 
-    addRating(value){
-        this._ratings.push(value);
-    }
+  addRating(value) {
+    this._ratings.push(value);
+  }
 }
 
-class Book extends Media{
-    constructor(author,title, pages, isCheckedOut, ratings){
-        super(title, isCheckedOut, ratings);
-        this._author = author;
-        this._pages = pages;
-    }
+class Book extends Media {
+  constructor(author, title, pages, isCheckedOut, ratings) {
+    super(title, isCheckedOut, ratings);
+    this._author = author;
+    this._pages = pages;
+  }
 
-    get author(){
-        return this._author;
-    };
+  get author() {
+    return this._author;
+  }
 
-    get title(){
-        return this._title;
-    };
+  get title() {
+    return this._title;
+  }
 
-    get pages(){
-        return this._pages;
-    }
+  get pages() {
+    return this._pages;
+  }
 
-    get isCheckedOut(){
-        return this._isCheckedOut;
-    }
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
 
-    get ratings(){
-        return this._ratings;
-    }
+  get ratings() {
+    return this._ratings;
+  }
 
-    getAverageRating(){
-        let ratingsSum = this._ratings.reduce((currentSum, rating) => currentSum + rating,0);
-        return ratingsSum / this._ratings.length;
-    };
+  getAverageRating() {
+    let ratingsSum = this._ratings.reduce(
+      (currentSum, rating) => currentSum + rating,
+      0
+    );
+    return ratingsSum / this._ratings.length;
+  }
 
-    toggleCheckOutStatus(){
-        this._isCheckedOut = !this._isCheckedOut;
-    };
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this._isCheckedOut;
+  }
 
-    addRating(value){
-        this._ratings.push(value);
-    };
+  addRating(value) {
+    this._ratings.push(value);
+  }
 }
 
-class Movie extends Media{
-    constructor(director,title, runTime, isCheckedOut, ratings){
-        super(title, isCheckedOut, ratings);
-        this._director = director;
-        this._runTime = runTime;
-        }
+class Movie extends Media {
+  constructor(director, title, runTime, isCheckedOut, ratings) {
+    super(title, isCheckedOut, ratings);
+    this._director = director;
+    this._runTime = runTime;
+  }
 
-    get director(){
-        return this._director;
-    };
+  get director() {
+    return this._director;
+  }
 
-    get title(){
-        return this._title;
-    };
+  get title() {
+    return this._title;
+  }
 
-    get runTime(){
-        return this._runTime;
-    }
+  get runTime() {
+    return this._runTime;
+  }
 
-    get isCheckedOut(){
-        return this._isCheckedOut;
-    }
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
 
-    get ratings(){
-        return this._ratings;
-    }
+  get ratings() {
+    return this._ratings;
+  }
 
-    getAverageRating(){
-        let ratingsSum = this._ratings.reduce((currentSum, rating) => currentSum + rating,0);
-        return ratingsSum / this._ratings.length;
-    };
+  getAverageRating() {
+    let ratingsSum = this._ratings.reduce(
+      (currentSum, rating) => currentSum + rating,
+      0
+    );
+    return ratingsSum / this._ratings.length;
+  }
 
-    toggleCheckOutStatus(){
-        this._isCheckedOut = !this._isCheckedOut;
-    };
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this._isCheckedOut;
+  }
 
-    addRating(value){
-        this._ratings.push(value);
-    };
+  addRating(value) {
+    this._ratings.push(value);
+  }
 }
 
-const historyOfEverything = new Book('Bill Bryson', "A Short History of Nearly Everything", 544);
+const historyOfEverything = new Book(
+  "Bill Bryson",
+  "A Short History of Nearly Everything",
+  544
+);
 
 historyOfEverything.toggleCheckOutStatus();
 
